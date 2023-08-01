0b6eec81f0be5600d31d2da72e38c5ea0feb121e
Santiago Laredo <slaredo@ragasa.com.mx>
2023-08-01 12:10:14 -0600

    Mod prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,14 @@
+
 export const environment = {
   production: true,
-  // rest_api_url: 'http://128.199.41.36:3000',
-  // graphql_api_url: 'http://128.199.41.36:3000/admin',
-  rest_api_url: 'http://localhost:3001',
-  graphql_api_url: 'http://localhost:3001/admin',
-  aws_s3_api: 'https://jbweb-crm-admin.s3.amazonaws.com',
+  rest_api_url: 'http://146.190.129.32/:3001',
+  graphql_api_url: 'http://146.190.129.32/:3001/admin',
   authTokenKey: 'Production',
   version: '1.0.0',
   aws_file_service: {
-    access_key_id: 'AKIA5UTPAFHAT72YVYVZ',
-    secret_access_key: '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',
+    access_key_id: 'AKIA5UTPAFHAURLX2DOE',
+    secret_access_key: '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',
     region: 'us-east-1',
-    bucket: 'jbweb-crm-admin'
+    bucket: 'jarboss-web-crm-admin'
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: true,
-  // rest_api_url: 'http://52.117.21.213:3001',
-  // graphql_api_url: 'http://52.117.21.213:3001/admin',
-  rest_api_url: 'http://52.117.21.213:3001',
-  graphql_api_url: 'http://52.117.21.213:3001/admin',
+  rest_api_url: 'http://146.190.129.32/:3001',
+  graphql_api_url: 'http://146.190.129.32/:3001/admin',
   authTokenKey: 'Production',
   version: '1.0.0',
   aws_file_service: {
@@ -17,24 +12,3 @@ export const environment = {
     bucket: 'jarboss-web-crm-admin'
   }
 }
-
-// // This file can be replaced during build by using the `fileReplacements` array.
-// // `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// // The list of file replacements can be found in `angular.json`.
-
-// export const environment = {
-//   production: true,
-//   // rest_api_url: 'http://128.199.41.36:3000',
-//   // graphql_api_url: 'http://128.199.41.36:3000/admin',
-//   rest_api_url: 'http://localhost:3001',
-//   graphql_api_url: 'http://localhost:3001/admin',
-//   aws_s3_api: 'https://crm.s3.us-east-1.amazonaws.com/',
-//   authTokenKey: 'Production',
-//   version: '1.0.0',
-//   aws_file_service: {
-//     access_key_id: 'AKIAZTHYD2V6E5G74EIP',
-//     secret_access_key: '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',
-//     region: 'us-east-1',
-//     bucket: 'nwfbucket'
-//   }
-// }
